1f31c7c9265e1a01f9ea534fc6b88ffaac119807
Josef Birman <josefbirman@gmail.com>
2024-10-28 15:10:47 -0400

    changed /new to get

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,9 +4,38 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_reader :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  #ADDED FUNCTIONS FOR GAME
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || !letter.match?(/[a-zA-Z]/)
+    letter.downcase!
+
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    if @word.include?(letter)
+      @guesses << letter
+      true
+    else
+      @wrong_guesses << letter
+      true
+    end
+  end
+
+  def word_with_guesses
+    @word.chars.map { |char| @guesses.include?(char) ? char : '-' }.join
+  end
+
+  def check_win_or_lose
+    return :win if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
   end
 
   # You can test it by installing irb via $ gem install irb
